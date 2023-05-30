93a2d7911ecd895b4aeb5923171904ed7722db94
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-30 18:17:21 -0500

    migração de filmes

---
+++ FilmeApi/Migrations/20230530202406_tabelaDeFilme.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FilmeApi.Migrations
+{
+    public partial class tabelaDeFilme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    titulo = table.Column<string>(type: "text", nullable: false),
+                    genero = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    duracao = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
