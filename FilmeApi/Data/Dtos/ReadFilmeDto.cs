78e35eed5a6360c2b1b79daec9f2e6d5df7ea876
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-30 18:45:59 -0500

    criaddo dto para listar filmes

---
+++ FilmeApi/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,10 @@
+namespace FilmeApi.Data.Dtos;
+
+public class ReadFilmeDto
+{
+    
+    public string titulo { get; set; }
+    public string genero { get; set; }
+    public int duracao { get; set; }
+    public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+}
