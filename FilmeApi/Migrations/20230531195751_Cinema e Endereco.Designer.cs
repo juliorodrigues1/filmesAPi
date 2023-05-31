307bda8c58726c76c56b030650b2c01d630dac84
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-31 15:00:41 -0500

    migração da tabela de cinema e endereco

--- FilmeApi/Migrations/20230531195751_Cinema e Endereco.Designer.cs
+++ FilmeApi/Migrations/20230531195751_Cinema e Endereco.Designer.cs
@@ -2,6 +2,7 @@
 using FilmeApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FilmeApi.Migrations
 {
     [DbContext(typeof(FilmeApiContext))]
-    partial class FilmeApiContextModelSnapshot : ModelSnapshot
+    [Migration("20230531195751_Cinema e Endereco")]
+    partial class CinemaeEndereco
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FilmeApi/Migrations/20230531195751_Cinema e Endereco.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FilmeApi.Migrations
+{
+    public partial class CinemaeEndereco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enderecos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    logradouro = table.Column<string>(type: "text", nullable: false),
+                    numero = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enderecos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cinemas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    enderecoId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cinemas", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Cinemas_Enderecos_enderecoId",
+                        column: x => x.enderecoId,
+                        principalTable: "Enderecos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinemas_enderecoId",
+                table: "Cinemas",
+                column: "enderecoId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cinemas");
+
+            migrationBuilder.DropTable(
+                name: "Enderecos");
+        }
+    }
+}
