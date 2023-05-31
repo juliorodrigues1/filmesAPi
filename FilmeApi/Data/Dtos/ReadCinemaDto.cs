7b41a4c9ab4a11d0ff21ca5bb40c156f2251ffd5
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-31 16:09:36 -0500

    mudando nomeclatura de endereco

--- FilmeApi/Data/Dtos/ReadCinemaDto.cs
+++ FilmeApi/Data/Dtos/ReadCinemaDto.cs
@@ -4,5 +4,5 @@ public class ReadCinemaDto
 {
     public int id { get; set; }
     public string nome { get; set; }
-    public ReadEnderecoDto readEnderecoDto { get; set; }
+    public ReadEnderecoDto endereco { get; set; }
 }
