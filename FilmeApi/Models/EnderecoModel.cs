1af411d41806e2aeea45b0861df5838c752a8e0c
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-31 13:40:44 -0500

    criando model de endereco

---
+++ FilmeApi/Models/EnderecoModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmeApi.Models;
+
+public class EnderecoModel
+{
+    [Key]
+    [Required]
+    public int id { get; set; }
+    [Required(ErrorMessage = "O Campo logradouro é obrigatorio")]
+    public string logradouro { get; set; }
+    [Required(ErrorMessage = "O Campo número é obrigatorio")]
+    public int numero { get; set; }
+}
