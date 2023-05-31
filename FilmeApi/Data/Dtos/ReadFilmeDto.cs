f51cf58179cfec22e829c9f6fa27027fc88b8724
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-31 17:24:58 -0500

    adicionando relacionamento com sessao

--- FilmeApi/Data/Dtos/ReadFilmeDto.cs
+++ FilmeApi/Data/Dtos/ReadFilmeDto.cs
@@ -7,4 +7,5 @@ public class ReadFilmeDto
     public string genero { get; set; }
     public int duracao { get; set; }
     public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+    public ICollection<ReadSessaoDto> sessoes { get; set; }
 }
