6e119ffd51616840039f1babfd098f4b1dd6f281
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-31 17:22:03 -0500

    adicionando relacionamento com sessao

--- FilmeApi/Models/CinemaModel.cs
+++ FilmeApi/Models/CinemaModel.cs
@@ -10,6 +10,6 @@ public class CinemaModel
     [Required(ErrorMessage = "O campo nome é obrigatório")] 
     public string nome { get; set; }
     public int enderecoId { get; set; }
-
     public virtual EnderecoModel Endereco { get; set; }
+    public virtual ICollection<SessaoModel> Sessoes { get; set; }
 }
